8ebea16e5d85f235134cf01e5fdba50f076a81e3
April <18055219130@163.com>
2025-08-21 03:24:21 +0800

    Init commit. - 实现了基本的Makrdown渲染 - 实现了主页的构建 - 实现了大部分组件的编写 - 完成了 Header和Footer的实现 - 完整实现了对手机的适配

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,12 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+import preact from "@astrojs/preact";
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
+  integrations: [preact()],
+});
---
+++ src/pages/post/post-2.md
@@ -0,0 +1,12 @@
+---
+layout: ../../layouts/MarkdownPostLayout.astro
+title: 我的第二篇博客文章
+author: Astro 学习者
+description: "学习了一些 Astro 后，我根本停不下来！"
+image: "https://docs.astro.build/assets/arc.webp"
+date: 2022-07-08
+tags: ["astro", "blogging", "learning in public", "successes"]
+categories: ["hlw"]
+origin: true
+---
+在学习 Astro 大约一周后，我决定尝试些新的东西。我编写并导入了一个小组件！
